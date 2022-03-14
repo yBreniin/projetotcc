cb449d1714dbd7ed60f1c42664de8465c6e4614f
Your Name <you@example.com>
2022-03-14 14:15:00 -0300

    v1

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private menu: MenuController) {}
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+}
