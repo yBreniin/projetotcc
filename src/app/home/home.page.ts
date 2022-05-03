0130a8b36fac7714df1feec54ac227f6d7cdb42a
Hiro? <brenofprocopio@gmail.com>
2022-05-03 08:59:07 -0300

    1.58
    slides e animations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
+import { AnimationController, MenuController, ModalController } from '@ionic/angular';
 
 
 @Component({
@@ -7,9 +7,33 @@ import { MenuController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor(private menu: MenuController) {}
+export class HomePage implements AfterViewInit {
+  @ViewChild('title', { read: ElementRef, static: true }) title: ElementRef;
+  anim: Animation;
 
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
+  constructor(
+    private menu: MenuController,
+    private animationCtrl: AnimationController,
+    private modalController: ModalController
+  ) {}
+
+  ngAfterViewInit() {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const Animation = this.animationCtrl
+      .create()
+      .addElement(this.title.nativeElement)
+      .duration(1000)
+      .iterations(1)
+      .fromTo('transform', 'translateX(-100px)', 'translateX(0px)')
+      .fromTo('opacity', '0', '1');
+
+    Animation.play();
+  }
 
   openFirst() {
     this.menu.enable(true, 'first');
